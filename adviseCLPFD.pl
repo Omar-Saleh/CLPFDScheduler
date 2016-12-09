fe1d77fa4290180ab26a97b53abfbcc26a28d4e0
Omar Saleh <omarsaleh0106@gmail.com>
2016-12-09 17:22:50 +0200

    Changed data representation to allow for the use of more constraints, eliminating the need to use prolog's built-in backtrack searching

--- adviseCLPFD.pl
+++ adviseCLPFD.pl
@@ -1,47 +1,47 @@
 :-use_module(library(clpfd)).
 
 % lecture(CourseNum, Slot, Group)
-lecture1(1, 1, 1).
-lecture2(1, 2, 1).
+lecture1(1, [1,5]).
+lecture2(1, [2, 8]).
 
-lecture1(1, 5, 2).
-lecture2(1, 8, 2).
+% lecture1(1, 5, 2).
+% lecture2(1, 8, 2).
 
-lecture1(2, 10, 1).
-lecture2(2, 0, 1).
+lecture1(2, [10, 1]).
+lecture2(2, [0, 0]).
 
-lecture1(2, 1, 2).
-lecture2(2, 0, 2).
+% lecture1(2, 1, 2).
+% lecture2(2, 0, 2).
 
 % ------------
 
 % Tutorial(CourseNum, Slot, TutorialGroup)
-tutorial1(1, 5, 1).
-tutorial2(1, 25, 1).
+tutorial1(1, [5, 6]).
+tutorial2(1, [25, 12]).
 
-tutorial1(1, 6, 2).
-tutorial2(1, 12, 2).
+% tutorial1(1, 6, 2).
+% tutorial2(1, 12, 2).
 
-tutorial1(2, 13, 1).
-tutorial2(2, 0, 1).
+tutorial1(2, [13, 26]).
+tutorial2(2, [0, 0]).
 
-tutorial1(2, 26, 2).
-tutorial2(2, 0, 2).
+% tutorial1(2, 26, 2).
+% tutorial2(2, 0, 2).
 
 % ------------
 
 % Lab(CourseNum, Slot, TutorialGroup)
-lab1(1, 0, 1).
-lab2(1, 0, 1).
+lab1(1, [0, 0]).
+lab2(1, [0, 0]).
 
-lab1(1, 0, 2).
-lab2(1, 0, 2).
+% lab1(1, 0, 2).
+% lab2(1, 0, 2).
 
-lab1(2, 17, 1).
-lab2(2, 0, 1).
+lab1(2, [17, 30]).
+lab2(2, [0, 0]).
 
-lab1(2, 30, 2).
-lab2(2, 0, 2).
+% lab1(2, 30, 2).
+% lab2(2, 0, 2).
 
 % ------------
 
@@ -53,12 +53,18 @@ scheduleCourse(CourseList, L, Score):-
   CourseList = [H | T],
   L1 = [X1, X2, X3, X4, X5, X6],
   L1 ins 0..30,
-  lecture1(H, X1, Y1),
-  lecture2(H, X2, Y1),
-  tutorial1(H, X3, Y2),
-  tutorial2(H, X4, Y2),
-  lab1(H, X5, Y3),
-  lab2(H, X6, Y3),
+  lecture1(H, Lectures1),
+  lecture2(H, Lectures2),
+  tutorial1(H, Tutorials1),
+  tutorial2(H, Tutorials2),
+  lab1(H, Labs1),
+  lab2(H, Labs2),
+  element(Y1, Lectures1, X1),
+  element(Y1, Lectures2, X2),
+  element(Y2, Tutorials1, X3),
+  element(Y2, Tutorials2, X4),
+  element(Y3, Labs1, X5),
+  element(Y3, Labs2, X6),
   X1 #\= 0 #==> X1 #\= X2,
   X3 #\= 0 #==> X3 #\= X4,
   X5 #\= 0 #==> X5 #> X3 #/\ X5 #\= X6,
@@ -75,7 +81,8 @@ scheduleCourse(CourseList, L, Score):-
 scheduleCourses(L):-
   scheduleCourse([1, 2], L1, Score),
   delete(L1, 0, L),
-  all_different(L).
+  all_different(L),
+  labeling([], L).
 
 calculateScore(_, _, 0, 0).
 calculateScore(Y2, Y3, 1, Score):-
