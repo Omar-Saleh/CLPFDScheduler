c143cbaa0a268fc6ad3fd74085e1e0fb7493ba8b
Omar Saleh <omarsaleh0106@gmail.com>
2016-12-09 03:18:45 +0200

    Renamed myAbs perdicate and structured the pl file for readability

--- adviseCLPFD.pl
+++ adviseCLPFD.pl
@@ -7,6 +7,8 @@ lecture2(1, 2, 1).
 lecture1(1, 5, 2).
 lecture2(1, 8, 2).
 
+% ------------
+
 % Tutorial(CourseNum, Slot, TutorialGroup)
 tutorial1(1, 5, 1).
 tutorial2(1, 25, 1).
@@ -14,14 +16,19 @@ tutorial2(1, 25, 1).
 tutorial1(1, 6, 2).
 tutorial2(1, 12, 2).
 
+% ------------
 
+% Lab(CourseNum, Slot, TutorialGroup)
 lab1(1, 0, 1).
 lab2(1, 0, 1).
 
 lab1(1, 0, 2).
 lab2(1, 0, 2).
 
+% ------------
 
+% Score is calculated in the case that the course consists of a tutorial and a lab, it is calculated as the absloute difference between the tutorial Group
+% And the lab Group.
 scheduleCourse(L, Score):-
   L = [X1, X2, X3, X4, X5, X6],
   L ins 0..30,
@@ -38,9 +45,9 @@ scheduleCourse(L, Score):-
   X6 #\= 0 #==> X6 #= X5 + 1,
   X5 #\= 0 #/\ X3 #\= 0 #==> Execute #= 1,
   X5 #= 0 #\/ X3 #= 0 #==> Execute #= 0,
-  myAbs(Y2, Y3, Execute, Score).
+  calculateScore(Y2, Y3, Execute, Score).
 
-myAbs(_, _, 0, 0).
+calculateScore(_, _, 0, 0).
 
-myAbs(Y2, Y3, 1, Score):-
+calculateScore(Y2, Y3, 1, Score):-
   abs(Y2 - Y3) #= Score.
