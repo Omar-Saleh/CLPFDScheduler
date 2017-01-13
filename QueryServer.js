a9f3d02e2e3dc6874ba3a5040594340fb6de27d6
Omar Ali <omar.moh.aly@gmail.com>
2017-01-13 21:10:54 +0200

    Prolog facts are printed to the console

--- QueryServer.js
+++ QueryServer.js
@@ -10,7 +10,7 @@ lectureMap = {};
 labMap = {};
 groupMap = {};
 
-fs.readFile('C:/Users/Omar/github/CLPFDScheduler/courses.csv', 'utf8', function (err,data) {
+fs.readFile('courses.csv', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
@@ -39,27 +39,237 @@ function mapScheduleToUniqueNumbers(listOfCourses) {
 
     switch (listOfCourses[i]['type']) {
       case 'Lab':
-        labMap[courseMap[listOfCourses[i]['course_code']]].push([listOfCourses[i]['day'], listOfCourses[i]['slot'], group]);
+        labMap[courseMap[listOfCourses[i]['course_code']]].push([group, (listOfCourses[i]['day'] - 1) * 6 + parseInt(listOfCourses[i]['slot'])]);
         break;
       case 'Tut':
-        tutorialMap[courseMap[listOfCourses[i]['course_code']]].push([listOfCourses[i]['day'], listOfCourses[i]['slot'], group]);
+        tutorialMap[courseMap[listOfCourses[i]['course_code']]].push([group,(listOfCourses[i]['day']- 1) * 6 + parseInt(listOfCourses[i]['slot'])]);
         break;
-      case 'Lec':
-        lectureMap[courseMap[listOfCourses[i]['course_code']]].push([listOfCourses[i]['day'], listOfCourses[i]['slot'], group]);
+      case 'Lecture':
+        lectureMap[courseMap[listOfCourses[i]['course_code']]].push([group, (listOfCourses[i]['day'] - 1) * 6 + parseInt(listOfCourses[i]['slot'])]);
         break;
       default:
         break;
     }
   }
+
+  // var a = listOfCourses
+
+
+
   // console.log(listOfCourses.length);
-  // console.log(courseMap);
-  // console.log(lectureMap);
-  // console.log(labMap);
-  // console.log(tutorialMap);
-  console.log(groupMap);
+  console.log(courseMap);
+  console.log(lectureMap);
+  // console.log(labMap[4]);
+  // console.log(tutorialMap[4]);
+  // console.log(groupMap);
   // console.log(listOfCourses[0]['group']);
   // console.log(listOfCourses[0]['group'].replace(/[^\d]/g, ''));
+
+  for(var i = 1; i < Object.keys(courseMap).length + 1; i++)
+  {
+    var lecs = lectureMap[i];
+    var tuts = tutorialMap[i];
+    var labs = labMap[i];
+
+
+    tuts1 = [];
+    tuts2 = [];
+    lecs1 = [];
+    lecs2 = [];
+    labs1 = [];
+    labs2 = [];
+
+    // console.log(lecs);
+// if(i == 4){
+//           // console.log("hiii");
+//           console.log(tuts);
+//           console.log(labs);
+//         }
+
+    
+  if(tuts.length != 0)
+  {
+     if(tuts[0][0] != tuts[1][0])
+      {
+        for(var j = 0; j < tuts.length; j++)
+        {
+          tuts1 = tuts1.concat([tuts[j][1]]);
+        }
+      }
+      else
+      {
+        for(var j = 0; j < tuts.length; j++)
+        {
+          if(j % 2 == 0)
+          {
+            tuts1 = tuts1.concat([tuts[j][1]]);
+          }
+          else
+          {
+            tuts2 = tuts2.concat([tuts[j][1]]);
+          } 
+        }
+      }
+  }
+
+  if(labs.length != 0)
+  {
+
+     if(labs[0][0] != labs[1][0])
+      {
+        for(var j = 0; j < labs.length; j++)
+        {
+          labs1 = labs1.concat([labs[j][1]]);
+        }
+      }
+      else
+      {
+        for(var j = 0; j < labs.length; j++)
+        {
+          if(j % 2 == 0)
+          {
+            labs1 = labs1.concat([labs[j][1]]);
+          }
+          else
+          {
+            labs2 = labs2.concat([labs[j][1]]);
+          } 
+        }
+      }
+  }
+
+  if(lecs.length != 0)
+  {
+      if(i == 13)
+      {
+        console.log(lecs);
+        console.log(lecs[1][0]);
+      }
+     if( lecs.length > 1 && lecs[0][0] != lecs[1][0])
+      {
+        for(var j = 0; j < lecs.length; j++)
+        {
+          lecs1 = labs1.concat([lecs[j][1]]);
+        }
+      }
+      else
+      {
+        for(var j = 0; j < lecs.length; j++)
+        {
+          if(j % 2 == 0)
+          {
+            lecs1 = lecs1.concat([lecs[j][1]]);
+          }
+          else
+          {
+            lecs2 = lecs2.concat([lecs[j][1]]);
+          } 
+        }
+      }
+  }
+
+  
+
+  console.log("lecture1(" + i + ", [" + lecs1 + "]).");
+    console.log("lecture2(" + i + ", [" + lecs2 + "]).");
+    console.log("tutorial1(" + i + ", [" + tuts1 + "]).");
+    console.log("tutorial2(" + i + ", [" + tuts2 + "]).");
+    console.log("labs1(" + i + ", [" + labs1 + "]).");
+    console.log("labs2(" + i + ", [" + labs2 + "]).");
+
+
+
+    // if(tuts.length != 0 && labs.length != 0)
+    // {
+    //   // console.log(tuts);
+    //     for(var j = 0; j < tuts.length; j++)
+    //     {
+    //       tuts1 = tuts1.concat([tuts[j][1]]);
+    //       labs1 = labs1.concat([labs[j][1]]);
+    //     }
+    // }
+    // else if(tuts.length != 0 && labs.length == 0)
+    // {
+      
+    //   if(tuts[0][0] != tuts[1][0])
+    //   {
+    //     for(var j = 0; j < tuts.length; j++)
+    //     {
+    //       tuts1 = tuts1.concat([tuts[j][1]]);
+    //     }
+    //   }
+    //   else
+    //   {
+    //     for(var j = 0; j < tuts.length; j++)
+    //     {
+    //       if(j % 2 == 0)
+    //       {
+    //         tuts1 = tuts1.concat([tuts[j][1]]);
+    //       }
+    //       else
+    //       {
+    //         tuts2 = tuts2.concat([tuts[j][1]]);
+    //       } 
+    //     }
+    //   }
+    // }
+    // else if(labs.length != 0 && tuts.length == 0)
+    // {
+
+    //   if(labs[0][0] != labs[1][0])
+    //   {
+
+    //     for(var j = 0; j < labs.length; j++)
+    //     {
+    //       labs1 = labs1.concat([labs[j][1]]);
+    //     }
+    //   }
+    //   else
+    //   {
+
+    //    for(var j = 0; j < labs.length; j++)
+    //     {
+    //       if(j % 2 == 0)
+    //       {
+    //         labs1 = labs1.concat([labs[j][1]]);
+    //       }
+    //       else
+    //       {
+    //         labs2 = labs2.concat([labs[j][1]]);
+    //       } 
+    //     }
+    //   }
+    // }
+    // else
+    // {
+    //   console.log("hello");
+    // }
+
+
+
+    
+
+
+
+
+
+
+
+
+  }
+
+
+
+
+
+  // var file = new File("facts.txt");
+  // file.open("w");
+  // file.writeln("hello!");
+  // file.close();
+
+
 }
+
 
 //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
 // var options = {
@@ -83,3 +293,5 @@ function mapScheduleToUniqueNumbers(listOfCourses) {
 // }
 //
 // http.request(options, callback).end();
+
+
